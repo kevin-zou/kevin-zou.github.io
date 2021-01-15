d8af1c86f3eb3b42978391d98bfde93f5eca09ff
Kevin Zou <thewebproducer@gmail.com>
2021-01-14 22:30:03 -0800

    Save theme state in local storage

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 window.onload = function () {
-  // Dark mode toggle
-  let darkMode = false;
-  const darkModeButton = $('#dark-mode-toggle');
-  darkModeButton.click(function () {
-    darkMode = !darkMode;
+  // Check if dark mode already enabled
+  let darkMode = localStorage.getItem('theme') === 'dark';
+
+  if (darkMode) {
+    $('body').toggleClass('dark-mode');
+  }
+
+  // Add listener for theme toggle button
+  const themeToggle = $('#theme-toggle');
+  themeToggle.click(function () {
     $('body').toggleClass('dark-mode');
     $(this).text(`Turn ${darkMode ? 'on' : 'off'} the lights`);
+    localStorage.setItem("theme", darkMode ? "light" : "dark");
   });
 }
