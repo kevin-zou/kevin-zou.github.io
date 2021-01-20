d90e11b06946b394b420993d127954393d611609
Kevin Zou <thewebproducer@gmail.com>
2021-01-19 18:17:59 -0800

    Fix dark mode toggle

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ window.onload = function () {
 
   if (darkMode) {
     $('body').toggleClass('dark-mode');
+    $('#theme-toggle').text('Turn on the lights');
   }
 
   // Add listener for theme toggle button
   const themeToggle = $('#theme-toggle');
   themeToggle.click(function () {
+    darkMode = !darkMode;
     $('body').toggleClass('dark-mode');
     $(this).text(`Turn ${darkMode ? 'on' : 'off'} the lights`);
-    localStorage.setItem("theme", darkMode ? "light" : "dark");
+    localStorage.setItem("theme", darkMode ? "dark" : "light");
   });
 }
